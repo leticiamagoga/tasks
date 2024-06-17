f0328f9e004b85d3936479b4666012bb33d46db0
leticiamagoga <leticiamagogasfs@gmail.com>
2024-06-17 08:50:03 -0300

    alteraçoes taks list

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,22 @@
 import react from "react"
-import {Text, View, StyleSheet} from "react-native"
+import {Text, View, StyleSheet, TouchableWithoutFeedback} from "react-native"
 import moment from "moment"
+import { Icon } from "react-native-vector-icons/FontAwesome6"
 
 export default props => {
     return(
         <View style={styles.container}>
+        <TouchableWithoutFeedback >{/*elemento tocavel faz alteraçoes com toque*/}
+        <View>
+        <Icon name="check" size={20} color="#FFF"/>
+
+        </View>
+
+        </TouchableWithoutFeedback>
             <View>
-                <Text>{props.description}</Text>
-                <Text>{props.estimate_at}</Text>
-                <Text>{props.done_at}</Text>
+                <Text style={styles.desc}>{props.description}</Text>
+                <Text style={styles.date}>{props.estimate_at + ""}</Text>
+                <Text style={styles.date}>{props.done_at +""}</Text>
             </View>
         </View>
     )
@@ -20,5 +28,38 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10
+    },
+    checkContainer:{
+        width:"20%",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    done:{
+        height: 25,
+        width:20,
+        borderRadius: 13,
+        borderWidth:1,
+        backgroundColor: "#4D7031",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    pending:{
+        height: 25,
+        width:20,
+        borderRadius: 13,
+        borderWidth:1,
+       borderColor: "#555"
+    },
+    desc:{
+        fontFamily: "Ariel",
+        fontSize: 20,
+        color: "#333"
+
+    },
+    date:{
+        fontFamily: "Ariel",
+        color: "#333"
     }
+
+
 })
